62dcb3300db40d796afecb0db619e7b172321135
moudekarvivek <moudekarvivek@gmail.com>
2025-03-19 15:40:13 +0530

    Spring Profiles, Externalizing Configurations, Config server

--- food-service/src/main/java/com/substring/foodie/food/FoodServiceApplication.java
+++ food-service/src/main/java/com/substring/foodie/food/FoodServiceApplication.java
@@ -1,5 +1,8 @@
 package com.substring.foodie.food;
 
+import com.substring.foodie.food.config.TestConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,10 +11,17 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
-public class FoodServiceApplication {
+public class FoodServiceApplication implements CommandLineRunner {
+
+//	@Autowired
+//	private TestConfig testConfig;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodServiceApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		//		System.out.println(testConfig);
+	}
 }
--- food-service/src/main/java/com/substring/foodie/food/config/ProjectConfig.java
+++ food-service/src/main/java/com/substring/foodie/food/config/ProjectConfig.java
@@ -4,12 +4,19 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 @EnableFeignClients
 public class ProjectConfig {
+
+//    @Bean
+//    @Profile("prod")
+//    public TestConfig testConfig() {
+//        return new TestConfig();
+//    }
 
     @Bean
     @LoadBalanced
